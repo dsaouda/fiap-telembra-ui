cf7d5433e8b7de21033f783667c2207f04c7c15f
Diego Saouda <diego.saouda@forseit.com.br>
2017-10-10 12:21:00 -0300

    atualizacao do vuetifyjs

--- src/components/AppLayout.vue
+++ src/components/AppLayout.vue
@@ -1,14 +1,15 @@
 <template>
-    <v-app id="app" toolbar footer>
+    <v-app id="app">
         <v-navigation-drawer
-            class="pb-0"
-            persistent
+            temporary
             clipped
-            height="100%"
-            light
+            absolute
+            overflow
+            enable-resize-watcher
+            app
             v-model="drawer">
             <v-list dense>
-            
+
                 <v-list-tile>              
                     <v-list-tile-action>
                         <v-icon>home</v-icon>
@@ -121,15 +122,30 @@
         
         </v-navigation-drawer>
         
-        <v-toolbar class="indigo" dark fixed>
-            <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>      
+        <v-toolbar class="indigo" dark app absolute clipped-left>
+            <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>                 
             <slot name="title"></slot>
+            Fiap Telembra
         </v-toolbar>
         
         <main>
-            <v-container fluid>
-                <slot></slot>
-            </v-container>
+            <v-content>
+                <v-container fluid>
+                    <v-layout align-center justify-center>
+                        <v-flex xs10>
+                            <v-card>
+                                <v-card-text>
+                                    <v-layout row wrap>
+                                        <v-flex xs12 md12>                                        
+                                            <slot></slot>
+                                        </v-flex>
+                                    </v-layout>
+                                </v-card-text>
+                            </v-card>
+                        </v-flex>
+                    </v-layout>
+                </v-container>
+            </v-content>            
         </main>
         
     </v-app>
@@ -140,7 +156,7 @@
     name: 'app-layout',
     data () {
       return {
-        drawer: null
+        drawer: false
       }
     },
     methods: {
